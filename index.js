071de6a52dc54323593442a7a052bd39b44628cf
dlusion <dlusion@uw.edu>
2020-03-11 19:47:51 -0700

    push

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
     let data = ""
     let svg = ""
+    let tooltip = ""
 
     const measurements = {
         margin: 50,
@@ -11,19 +12,17 @@
         height: 500
     }
 
-    let tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .attr('style', 'position: absolute; opacity: 0;');
-
     window.onload = function() {
         svg = d3.select("body").append("svg")
             .attr("width", 700)
             .attr("height", 700);
 
-        d3.csv("data/listings2.csv", function (data){
-            // let price = data.map((row) => row["monthly_price"]);
-            console.log(typeof(data))
-            // makeHistogram(data)
+        tooltip = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .attr('style', 'position: absolute; opacity: 0;');
+
+        d3.csv("data/listings2.csv").then(function (data) {
+            makeHistogram(data)
         })
     }
 
@@ -76,100 +75,100 @@
             .attr("width", function(d) { return x(d.x1) - x(d.x0); })
             .attr("height", function(d) { return measurements.height - y(d.length); })
             .style("fill", "steelblue")
-            // .on("mouseover", function (d) {
-            //     data.filter(data => data["monthly_price"] >= d.x0 && data["monthly_price"] <= d.x1)
-            //     console.log(data)
-            //     // console.log(data.filter(data => data["monthly_price"]))
-            //     tooltip.transition()
-            //     .duration(200)
-            //     .style("opacity", 1);
+            .on("mouseover", function (d) {
+                data.filter(data => data["monthly_price"] >= d.x0 && data["monthly_price"] <= d.x1)
+                // console.log(data)
+                // console.log(data.filter(data => data["monthly_price"]))
+                tooltip.transition()
+                .duration(200)
+                .style("opacity", 1);
 
-            //     tooltip.style("left", (d3.event.pageX + 30) + "px")
-            //     .style("top", (d3.event.pageY - 120) + "px");
+                tooltip.style("left", (d3.event.pageX + 30) + "px")
+                .style("top", (d3.event.pageY - 120) + "px");
 
-            //     let tSvg = tooltip.append("svg")
-            //     .attr("height", "300px")
-            //     .attr("width", "325px")
+                let tSvg = tooltip.append("svg")
+                    .attr("height", "300px")
+                    .attr("width", "325px")
 
-            //     // Appends text containing the name of the country that was hovered over to the linegraph
-            //     tSvg.append("text")
-            //         .attr("text-anchor", "middle")
-            //         .attr("font-size", "16px")
-            //         .attr("x", (325) / 2)
-            //         .attr("y", (15))
-            //         .attr("fill", "black")
-            //         // .text(country);
+                // Appends text containing the name of the country that was hovered over to the linegraph
+                tSvg.append("text")
+                    .attr("text-anchor", "middle")
+                    .attr("font-size", "16px")
+                    .attr("x", (325) / 2)
+                    .attr("y", (15))
+                    .attr("fill", "black")
+                    // .text(country);
 
-            //     // nygeo data which contains information on the overall nyc map 
-            //     d3.json('./neighbourhoods.json').then(function(data) {
+                // nygeo data which contains information on the overall nyc map 
+                d3.json('data/neighbourhoods.geojson').then(function(mapdata) {
+                    console.log(mapdata)
+                    // // data which contains information on individual points
+                    // d3.csv('listings.csv').then(function(pointData) {
 
-            //         // // data which contains information on individual points
-            //         // d3.csv('listings.csv').then(function(pointData) {
+                        // scaling function to convert point data to map. Rotated to proper lat/long
+                        const albersProj = d3.geoAlbers()
+                            .scale(75000)
+                            .rotate([122.3321, 0])
+                            .center([0, 47.6062])
+                            .translate([325/2, 300/2]);
 
-            //             // scaling function to convert point data to map. Rotated to proper lat/long
-            //             const albersProj = d3.geoAlbers()
-            //                 .scale(75000)
-            //                 .rotate([74.0060, 0])
-            //                 .center([0, 40.7120])
-            //                 .translate([width/2, height/2]);
+                        // projects points onto the map
+                        const geoPath = d3.geoPath()
+                        .projection(albersProj)
 
-            //             // projects points onto the map
-            //             const geoPath = d3.geoPath()
-            //             .projection(albersProj)
+                        // grouping for paths which compose nyc map
+                        let sea = tSvg.append( "g" ).attr( "id", "sea" );
+                        sea.selectAll('path')
+                        .data(mapdata.features)
+                        .enter()
+                        .append('path')
+                            .attr('fill', '#878787')
+                            .attr('stroke', 'black')
+                            .attr('d', geoPath)
 
-            //             // grouping for paths which compose nyc map
-            //             let sea = tSvg.append( "g" ).attr( "id", "sea" );
-            //             nyc.selectAll('path')
-            //             .data(data.features)
-            //             .enter()
-            //             .append('path')
-            //                 .attr('fill', '#878787')
-            //                 .attr('stroke', 'black')
-            //                 .attr('d', geoPath)
-
-            //             // plots circles on the nyc map and adds on-click function 
-            //             // that transitions point to new direction + removes point from map.
-            //             let bnb = tSvg.append( "g" ).attr( "id", "bnb" );
-            //             bnb.selectAll('.circle')
-            //                 .data(data)
-            //                 .enter()
-            //                 .append('circle')
-            //                     .attr('cx', function(d) { 
-            //                         let scaledPoints = albersProj([d['longitude'], d['latitude']])
-            //                         return scaledPoints[0]
-            //                     })
-            //                     .attr('cy', function(d) {
-            //                         let scaledPoints = albersProj([d['longitude'], d['latitude']])
-            //                         return scaledPoints[1]
-            //                     })
-            //                     .attr('r', 4)
-            //                     .attr('fill', 'steelblue')
-            //                     .attr("stroke", "black")
-            //                     .on( "click", function(){
-            //                         let thing = d3.select(this)
-            //                             .attr("opacity", 1)
-            //                             .transition()
-            //                                 .duration( 2000 )
-            //                                 .attr( "cy", 1000 )
-            //                                 .attr( "opacity", 0 )
-            //                                 .on("end", function(thing) {
-            //                                     d3.select(this).remove();
-            //                                 })
-            //                     });
+                        // plots circles on the nyc map and adds on-click function 
+                        // that transitions point to new direction + removes point from map.
+                        // let bnb = tSvg.append( "g" ).attr( "id", "bnb" );
+                        // bnb.selectAll('.circle')
+                        //     .data(data)
+                        //     .enter()
+                        //     .append('circle')
+                        //         .attr('cx', function(d) { 
+                        //             let scaledPoints = albersProj([d['longitude'], d['latitude']])
+                        //             return scaledPoints[0]
+                        //         })
+                        //         .attr('cy', function(d) {
+                        //             let scaledPoints = albersProj([d['longitude'], d['latitude']])
+                        //             return scaledPoints[1]
+                        //         })
+                        //         .attr('r', 4)
+                        //         .attr('fill', 'steelblue')
+                        //         .attr("stroke", "black")
+                        //         .on( "click", function(){
+                        //             let thing = d3.select(this)
+                        //                 .attr("opacity", 1)
+                        //                 .transition()
+                        //                     .duration( 2000 )
+                        //                     .attr( "cy", 1000 )
+                        //                     .attr( "opacity", 0 )
+                        //                     .on("end", function(thing) {
+                        //                         d3.select(this).remove();
+                        //                     })
+                        //         });
 
                         
-            //         // })
+                    // })
                 
-            //     })
-            // })
-            // .on("mouseout", function (d) {
-            //     tooltip.transition()
-            //         .duration(0)
-            //         .style("opacity", 0)
-            //     tooltip.selectAll("svg")
-            //         .attr("height", "0px")
-            //         .attr("width", "0px")
-            // });
+                })
+            })
+            .on("mouseout", function (d) {
+                tooltip.transition()
+                    .duration(0)
+                    .style("opacity", 0)
+                tooltip.selectAll("svg")
+                    .attr("height", "0px")
+                    .attr("width", "0px")
+            });
         }
 
     function drawAxes(scaleX, scaleY) {
